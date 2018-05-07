a0ae6767c1b30e091afa446d392b36d6ce5f8907
rbonnet <rbonnet@FRL0437.fr.devoteam.com>
2018-05-07 09:23:03 +0200

    up

--- src/main/java/org/jboss/tools/example/springmvc/controller/HomeController.java
+++ src/main/java/org/jboss/tools/example/springmvc/controller/HomeController.java
@@ -12,6 +12,6 @@ public class HomeController {
 	@RequestMapping(value = "/")
 	public String home() {
 		System.out.println("HomeController: Passing through...");
-		return "webapp/views/index.jsp";
+		return "webapp/views/welcome.jsp";
 	}
 }
